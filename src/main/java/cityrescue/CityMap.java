ae0b5b6d84fdc001db5ba3a0513faa685012b712
lthompson233 <lt677@exeter.ac.uk>
2026-03-04 22:41:06 +0000

    test

--- src/main/java/cityrescue/CityMap.java
+++ src/main/java/cityrescue/CityMap.java
@@ -10,5 +10,99 @@ package cityrescue;
  * @author cosmofolder
  */
 public class CityMap {
+    
+    private int width;
+    private int height;
+    private CitySquare[][] map;
 
+    public void initialise(int width, int height){
+
+        
+        // clear all stations / units / incidents
+        // tick = 0
+        // obstacles emptied
+
+        map = new CitySquare[height][width];
+
+
+        this.width = width;
+        this.height = height;
+
+        if (width > 0 && height > 0){
+            for (int y = 0; y < height; y++){
+                for (int x = 0; x < width; x++){
+                    map[y][x] = new CitySquare();
+                    map[y][x].initialise(x, y);
+                    System.out.println(x + " : " + y);
+                }
+            }
+        }
+        else{
+            // width < 0 or height < 0
+        }
+
+        
+
+        
+
+    }
+
+    public int[] getGridSize(){
+        return new int[] { width, height };
+    }
+
+    /** 
+    public void addObstacle(Obstacle obstacle){
+        // add obstacle
+    }
+
+    public void removeObstacle(Obstacle obstacle){
+        // remove obstacle
+    }
+    */
+
+    public int addStation(Station station){
+        // add station
+        return -1;
+    }
+
+    public void removeStation(Station station){
+        // remove station
+    }
+
+    public int addUnit(Unit unit){
+        // add unit
+
+        
+
+        return -1;
+    }
+
+    public void removeUnit(Unit unit){
+        // remove unit
+    }
+
+    public int addIncident(Incident incident){
+        // add incident
+        
+        return -1;
+    }
+
+    public void removeIncident(Incident incident){
+        // remove incident
+    }
+
+
+}
+
+
+class Temp {
+    public static void main(String[] args)
+    {
+        CityMap cityMap;
+        cityMap = new CityMap();
+        cityMap.initialise(5, 5);
+        System.out.println("Fin");
+    
+    }
 }
---
+++ src/main/java/cityrescue/CitySquare.java
@@ -0,0 +1,20 @@
+package cityrescue;
+
+public class CitySquare{
+    private int x;
+    private int y;
+
+    private Unit[] unit;
+    private Incident incident;
+    private Station station;
+    
+    // private Obstical obstical
+
+    public void initialise(int x, int y){
+        this.x = x;
+        this.y = y;
+    
+    }
+
+}
+
