30d0c83aa5d287761ec4517aaef186249b633520
lthompson233 <lt677@exeter.ac.uk>
2026-03-04 23:24:11 +0000

    stuf

--- src/main/java/cityrescue/CityMap.java
+++ src/main/java/cityrescue/CityMap.java
@@ -15,7 +15,7 @@ public class CityMap {
     private int height;
     private CitySquare[][] map;
 
-    public void initialise(int width, int height){
+    public CityMap(int width, int height){
 
         
         // clear all stations / units / incidents
@@ -31,8 +31,7 @@ public class CityMap {
         if (width > 0 && height > 0){
             for (int y = 0; y < height; y++){
                 for (int x = 0; x < width; x++){
-                    map[y][x] = new CitySquare();
-                    map[y][x].initialise(x, y);
+                    map[y][x] = new CitySquare(x, y);
                     System.out.println(x + " : " + y);
                 }
             }
@@ -47,6 +46,10 @@ public class CityMap {
 
     }
 
+    public CitySquare mapXY(int x, int y){
+        return map[y][x];
+    }
+
     public int[] getGridSize(){
         return new int[] { width, height };
     }
@@ -59,49 +62,50 @@ public class CityMap {
     public void removeObstacle(Obstacle obstacle){
         // remove obstacle
     }
-    */
 
-    public int addStation(Station station){
-        // add station
-        return -1;
-    }
-
-    public void removeStation(Station station){
-        // remove station
-    }
-
-    public int addUnit(Unit unit){
+    public void addUnit(Unit unit){
         // add unit
 
-        
-
-        return -1;
+        mapXY(unit.getx(), unit.gety()).setUnit(unit);
     }
 
     public void removeUnit(Unit unit){
         // remove unit
+
+        mapXY(unit.getx(), unit.gety()).removeUnit(unit);
     }
 
-    public int addIncident(Incident incident){
+    public void addIncident(Incident incident){
         // add incident
         
-        return -1;
+        mapXY(incident.getx(), incident.gety()).setIncident(incident);
     }
 
     public void removeIncident(Incident incident){
         // remove incident
+
+        mapXY(incident.getx(), incident.gety()).setIncident(null);
+    }
+    */
+
+    public void addStation(Station station){
+        // add station
+
+        mapXY(station.getx(), station.gety()).setStation(station);
     }
 
+    public void removeStation(Station station){
+        // remove station
 
+        mapXY(station.getx(), station.gety()).setStation(null);
+    }
 }
 
 
 class Temp {
     public static void main(String[] args)
     {
-        CityMap cityMap;
-        cityMap = new CityMap();
-        cityMap.initialise(5, 5);
+        CityMap cityMap = new CityMap(5,5);
         System.out.println("Fin");
     
     }
--- src/main/java/cityrescue/CitySquare.java
+++ src/main/java/cityrescue/CitySquare.java
@@ -1,19 +1,51 @@
 package cityrescue;
+import java.util.ArrayList;
 
 public class CitySquare{
     private int x;
     private int y;
 
-    private Unit[] unit;
+    private ArrayList<Unit> units;
     private Incident incident;
     private Station station;
     
     // private Obstical obstical
 
-    public void initialise(int x, int y){
+    public CitySquare(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
     
+
+    public ArrayList<Unit> getUnits(){
+        return units;
+    }
+
+    public Incident getIncident(){
+        return incident;
+    }
+
+    public Station getStation(){
+        return station;
+    }
+
+    
+
+    public void addUnit(Unit unit){
+        units.add(unit);
+    }
+
+    public void removeUnit(Unit unit){
+        units.remove(unit);
+    }
+
+    public void setStation(Station station){
+        this.station = station;
+    }
+
+    public void setIncident(Incident incident){
+        this.incident = incident;
     }
 
 }
